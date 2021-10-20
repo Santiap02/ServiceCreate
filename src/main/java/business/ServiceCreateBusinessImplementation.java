338bcfa81b67f89dcad7923ce82175c66125a9b6
santiap03 <santiago.alvarezp@pragma.com.co>
2021-10-19 23:18:15 -0500

    Loggers.

--- src/main/java/business/ServiceCreateBusinessImplementation.java
+++ src/main/java/business/ServiceCreateBusinessImplementation.java
@@ -42,6 +42,7 @@ public class ServiceCreateBusinessImplementation implements ServiceCreateBusines
                 response = new ResponseDto<String>(HttpStatus.BAD_REQUEST.value(), ServiceConstants.SA003, ServiceConstants.SA003M);
             }
         }catch (Exception e){
+            LOGGER.error("Error in getClients", e);
             response = new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ServiceConstants.SA100, ServiceConstants.SA100M);
         }
         LOGGER.debug("AgregarCliente retorna: "+response);
@@ -52,6 +53,7 @@ public class ServiceCreateBusinessImplementation implements ServiceCreateBusines
 
     @Override
     public ResponseDto<String> addPhoto(int ClientId, MultipartFile file) throws IOException {
+        LOGGER.debug("Se inicia addPhoto");
         Photo photo = new Photo(ClientId);
         photo.setImage(new Binary(BsonBinarySubType.BINARY, file.getBytes()));
         ResponseDto<String> response = new ResponseDto<>();
@@ -59,7 +61,7 @@ public class ServiceCreateBusinessImplementation implements ServiceCreateBusines
         try {
             if(photoRepository.findByClientId(ClientId)==null){
                 photo = photoRepository.insert(photo);
-                response = new ResponseDto<String>(HttpStatus.OK.value(), ServiceConstants.SA002, ServiceConstants.SA002M, "MongoId de la imagen"+photo.getClientId());
+                response = new ResponseDto<String>(HttpStatus.OK.value(), ServiceConstants.SA002, ServiceConstants.SA002M, "MongoId de la imagen: "+photo.getId());
 
             }
             else{
@@ -67,8 +69,9 @@ public class ServiceCreateBusinessImplementation implements ServiceCreateBusines
             }
         } catch (Exception e) {
             response = new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ServiceConstants.SA100, ServiceConstants.SA100M);
-            e.printStackTrace();
+            LOGGER.error("Error in getClients", e);
         }
+        LOGGER.debug("addPhoto retorna: "+response);
         return  response;
 
     }
